80875c82d958a8c5d9849543a504c9f6e95c0528
joshua-miles <joshua@christetulit.org>
2019-04-09 22:17:47 -0500

    initial

---
+++ 27-iterators/server/db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Character.create( name: 'Rochell', x: 100, y: 100 )
+Character.create( name: 'Adam', x: 100, y: 100 )
+Character.create( name: 'Josie', x: 100, y: 100 )
+Character.create( name: 'Mark', x: 100, y: 100 )
+Character.create( name: 'Trey', x: 100, y: 100 )
+
+Character.create( name: 'Jessica', x: 100, y: 100 )
+Character.create( name: 'Olivia', x: 100, y: 100 )
+Character.create( name: 'Jing', x: 100, y: 100 )
+Character.create( name: 'Zoe', x: 100, y: 100 )
+Character.create( name: 'TK', x: 100, y: 100 )
+
+Character.create( name: 'Jack', x: 100, y: 100 )
+Character.create( name: 'Eli', x: 100, y: 100 )
+Character.create( name: 'Bobby', x: 100, y: 100 )
+Character.create( name: 'Cory', x: 100, y: 100 )
+Character.create( name: 'Hema', x: 100, y: 100 )
