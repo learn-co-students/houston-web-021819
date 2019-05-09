f1f4105d8252c0b806d2656ff8f707be92cf6f98
joshua-miles <joshua@christetulit.org>
2019-05-09 17:47:06 -0500

    -

--- 40-intro-to-redux/dragons-vs-hogs/src/characters/GalaxyNote.js
+++ 40-intro-to-redux/dragons-vs-hogs/src/characters/GalaxyNote.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { HealthBar } from '../components/HealthBar';
+import { connect } from 'react-redux'
 
-export class GalaxyNote extends Component {
-    render() {
-        return (
-            <div style={{ textAlign: 'right'}}>
-                <h3>GalaxyNote</h3>
-                <HealthBar />
-                <img style={{ width:'100px'}} src="https://res.cloudinary.com/dabeyvbyw/image/upload/v1544712623/galaxy_note.jpg" />
-            </div>
-        );
+const mapStateToProps = state => {
+    return {
+        health: state.galaxyNote
     }
 }
+
+const mapDispatchToProps = {
+
+}
+
+export const GalaxyNote = connect(mapStateToProps, mapDispatchToProps)( (props) => (
+    <div style={{ textAlign: 'right'}}>
+        <h3>GalaxyNote</h3>
+        <HealthBar health={props.health} />
+        <img style={{ width:'100px'}} src="https://res.cloudinary.com/dabeyvbyw/image/upload/v1544712623/galaxy_note.jpg" />
+    </div>
+))
